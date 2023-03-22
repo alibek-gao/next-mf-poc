a342df23834a313ac9a206084514c132e6fe31d4
Alibek Kazhibekov <alibek.gao@gmail.com>
2023-03-22 18:57:04 +0100

    feat: add NextFederationPlugin in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
+const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, { isServer }) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'poc',
+        filename: 'static/chunks/remoteEntry.js',
+        extraOptions: {
+          automaticAsyncBoundary: true,
+        },
+        exposes: {},
+        shared: {},
+      })
+    );
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
